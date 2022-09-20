c3a85774babcd1ac771b82c3b3fa1d006d5bb402
Mpholo Leboea <mpholo.leboea@gmail.com>
2022-09-20 21:26:36 +0200

    first commit

---
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springframework.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TsbbSfgBreweryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
